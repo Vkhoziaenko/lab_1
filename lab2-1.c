feb321727a3dc9fcc4fe898c2d530c8b23dc8588
Vkhoziaenko <55573780+Vkhoziaenko@users.noreply.github.com>
2019-10-17 23:28:02 +0300

    Add files via upload

--- lab2-1.c
+++ lab2-1.c
@@ -4,9 +4,9 @@
 int main(){
 	setlocale( LC_ALL, "Russian");
 	float a, b, c;
-	printf("Введите стороны");
+	printf("enter the parties");
 	scanf("%f%f%f",&a,&b,&c);
-	(a==b && b==c && c==a)? printf("Треугольник равносторонний"):printf("Треугольник неравносторонний");
+	(a==b && b==c && c==a)? printf("Equilateral triangle"):printf("The triangle is non-equilateral");
 	return 0;
 	
 }
