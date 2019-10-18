73735e88685ae8e212307f52e1af5b6b718d5135
Vkhoziaenko <55573780+Vkhoziaenko@users.noreply.github.com>
2019-10-18 09:16:58 +0300

    Add files via upload

--- lab2-3.c
+++ lab2-3.c
@@ -7,7 +7,7 @@ int main(int argc, char *argv[]) {
 	float a, b;
 	switch (argc){
 	
-	case 1:
+	case 1 :
 	printf("insert the number a:");
 	scanf("%f",&a);
 	printf("insert the number b:");
@@ -19,6 +19,18 @@ int main(int argc, char *argv[]) {
 	printf("insert the number b:");
 	scanf("%f",&b);
 	printf("the difference is %f ", a-b);
+    case 3:
+	printf("insert the number a:");
+	scanf("%f",&a);
+	printf("insert the number b:");
+	scanf("%f",&b);
+	printf("the the product of the numbers is %f ", a*b);
+	case 4: 
+	printf("insert the number a:");
+	scanf("%f",&a);
+	printf("insert the number b:");
+	scanf("%f",&b);
+	printf("the divide is %f ", a/b);
 	return 0;
 }
 }
